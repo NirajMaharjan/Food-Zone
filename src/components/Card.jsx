55e956ac1984f2cd22e212699bac1d5d208211a6
Niraj <maharjanniraj87@gmail.com>
2024-10-10 21:54:41 +0545

    responsive grids

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <Cards>
+      <img src={props.image} alt="" />
 
-
+      <div className="right">
+        <div className="top">
+          <p className="title">{props.title}</p>
+          <p className="description">{props.description}</p>
+        </div>
+        <Button text={props.price}/>
+      </div>
     </Cards>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
 
 const Cards = styled.div`
-    height: 167px;
-    width: 340px;
-    border: 0.66px #98F9FF;
-    background-color: #fff;
-    border-radius: 12px;
+  height: 167px;
+  width: 100%;
+  border: 0.66px #98f9ff;
+  background-color: rgba(78, 78, 78, 0.5); /* #7ED321 with 70% opacity */
+  backdrop-filter: blur(20px); /* Blur the background */
+  border-radius: 20px;
+  display: flex;
+  gap: 18px;
+  padding: 8px;
 
-`
+  .right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  img{
+    width: 133px;
+    height: 133px;
+    border-radius: 50%; /* This makes the image round */
+    object-fit: cover; /* Ensures the image doesn't stretch or get distorted */
+    display: block;
+  }
+
+  .title{
+
+font-size: 16px;
+font-weight: 600;
+line-height: 19.36px;
+text-align: left;
+
+  }
+
+  .top{
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+`;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,46 +1,50 @@
-import React,{useEffect,useState} from 'react'
-import styled from 'styled-components'
-import Card from './Card';
-
-
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "./Card";
 
 const Container = () => {
-    const [data,setData] = useState([]);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetch("https://run.mocky.io/v3/05cbb526-0d28-40ce-b825-ea4e9b8250fb")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setData(jsonData.foods);
+      }); //bcoz response is in object format {'foods':[{},{}...]}
+  }, []);
 
-    useEffect(() => {
-  fetch('https://run.mocky.io/v3/05cbb526-0d28-40ce-b825-ea4e9b8250fb')
-    .then(response => response.json())
-    .then(jsonData => {
-        setData(jsonData.foods)});  //bcoz response is in object format {'foods':[{},{}...]}
-        
-}, []);
+  return (
+    <Main>
 
     
-  return (  
-    <Main>
-        <ul>
+      {data.map((item, index) => (
+        <Card
+          key={index}
+          title={item.name}
+          price={item.price}
+          description={item.description}
+          image={item.image}
+        />
+      ))}
 
-        {data.map((item,index) => (
-            <li key={index}>{item.price}</li>
-        ))}
-        </ul>
-        <Card />
+     
     </Main>
-  )
-}
+  );
+};
 
-export default Container
-
+export default Container;
 
 const Main = styled.div`
-    width:100%;
-    height:100vh;
-    padding: 0;
-    margin: 0;
-    background-image: url('/images/bg.png'); /* Reference image from public folder */
+  width: 100%;
+  height: 100vh;
+  padding: 64px 120px 0px;
+  background-image: url("/images/bg.png"); /* Reference image from public folder */
   background-size: cover;
   background-position: center;
   color: white;
-
-`
+  display: grid;
+  grid-template-columns: repeat(auto-fit,minmax(340px,1fr));
+  grid-gap: 32px 20px;
+  justify-content: center;
+  justify-items: center;
+`;
